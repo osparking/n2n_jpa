8b28712df967a13be14891b43ea4b81827ebf508
osparking <jbpark03@gmail.com>
2024-06-23 08:44:50 +0900

    기본 생성자추가 - *Key 클래스

--- src/main/java/space/bum/jpa/n2n/entity/CourseRatingKey.java
+++ src/main/java/space/bum/jpa/n2n/entity/CourseRatingKey.java
@@ -7,11 +7,13 @@ import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
 @AllArgsConstructor
 @EqualsAndHashCode
+@NoArgsConstructor
 public class CourseRatingKey implements Serializable {
 
   private static final long serialVersionUID = 1L;
