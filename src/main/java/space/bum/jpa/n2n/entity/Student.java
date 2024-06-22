6e6d8a40bc5914610d08585e01aeb68699431176
osparking <jbpark03@gmail.com>
2024-06-22 10:59:13 +0900

    RDBMS 에 두 테이블간 관계를 표현

--- src/main/java/space/bum/jpa/n2n/entity/Student.java
+++ src/main/java/space/bum/jpa/n2n/entity/Student.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 
 @Entity
@@ -12,8 +14,14 @@ public class Student {
   @Id
   Long id;
 
+  // @formatter:off  
   @ManyToMany
+  @JoinTable(
+      name = "course_like", 
+      joinColumns = @JoinColumn(name = "student_id"), 
+      inverseJoinColumns = @JoinColumn(name = "course_id"))  
   Set<Course> likedCourses;
+  // @formatter:on
 
   // additional properties
   // standard constructors, getters, and setters
