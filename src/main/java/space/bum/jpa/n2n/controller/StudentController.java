fcf5080c026cdf3fed0a97f74821e8ff16611fa8
osparking <jbpark03@gmail.com>
2024-06-22 16:17:11 +0900

    제어기에서 학생 선호 과목 저장

--- src/main/java/space/bum/jpa/n2n/controller/StudentController.java
+++ src/main/java/space/bum/jpa/n2n/controller/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.AllArgsConstructor;
+import space.bum.jpa.n2n.entity.Course;
 import space.bum.jpa.n2n.entity.Student;
 import space.bum.jpa.n2n.repository.CourseRepository;
 import space.bum.jpa.n2n.repository.StudentRepository;
@@ -19,9 +20,12 @@ public class StudentController {
 
   @GetMapping("/")
   public String showHome() {
+    var course = courseRepository.save(new Course());
     var student = new Student();
-    var result = studentRepository.save(student);
 
+    student.getLikedCourses().add(course);
+    var result = studentRepository.save(student);
+    
     return "home";
   }
 
