5fc1f019816dd456e06ec035df48410582d84a6e
osparking <jbpark03@gmail.com>
2024-06-22 14:58:39 +0900

    학생 선호과목 멤버 메모리 확보

--- src/main/java/space/bum/jpa/n2n/entity/Student.java
+++ src/main/java/space/bum/jpa/n2n/entity/Student.java
@@ -1,5 +1,6 @@
 package space.bum.jpa.n2n.entity;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -9,8 +10,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import lombok.Data;
 
 @Entity
+@Data
 public class Student {
 
   @Id
@@ -23,7 +26,7 @@ public class Student {
       name = "course_like", 
       joinColumns = @JoinColumn(name = "student_id"), 
       inverseJoinColumns = @JoinColumn(name = "course_id"))  
-  Set<Course> likedCourses;
+  Set<Course> likedCourses = new HashSet<>();
   // @formatter:on
 
   // additional properties
