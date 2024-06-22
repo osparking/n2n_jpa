03926282e35ea092176ef266470085e9ec15a9d1
osparking <jbpark03@gmail.com>
2024-06-22 10:08:45 +0900

    프로젝트 초기 모습

---
+++ src/main/java/space/bum/security04/N2nJpaApplication.java
@@ -0,0 +1,13 @@
+package space.bum.security04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N2nJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N2nJpaApplication.class, args);
+	}
+
+}
