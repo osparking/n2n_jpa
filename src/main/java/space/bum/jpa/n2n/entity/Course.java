35b70ff2d1a0192e03f17c756f84e36ea6134e5f
osparking <jbpark03@gmail.com>
2024-06-22 14:54:11 +0900

    과목 엔티티 ID 에도 자동 생성 속성 명세

--- src/main/java/space/bum/jpa/n2n/entity/Course.java
+++ src/main/java/space/bum/jpa/n2n/entity/Course.java
@@ -3,6 +3,8 @@ package space.bum.jpa.n2n.entity;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 
@@ -10,6 +12,7 @@ import jakarta.persistence.ManyToMany;
 public class Course {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   Long id;
 
   @ManyToMany(mappedBy = "likedCourses")
