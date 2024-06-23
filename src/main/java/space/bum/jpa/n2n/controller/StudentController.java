f1c0be020a8ad408b8825818a5b1a5a3f8eb04d6
osparking <jbpark03@gmail.com>
2024-06-23 11:44:17 +0900

    학생의 과목 모의 등록 및 정보 저장

--- src/main/java/space/bum/jpa/n2n/controller/StudentController.java
+++ src/main/java/space/bum/jpa/n2n/controller/StudentController.java
@@ -1,5 +1,7 @@
 package space.bum.jpa.n2n.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,11 @@ import lombok.AllArgsConstructor;
 import space.bum.jpa.n2n.entity.Course;
 import space.bum.jpa.n2n.entity.CourseRating;
 import space.bum.jpa.n2n.entity.CourseRatingKey;
+import space.bum.jpa.n2n.entity.CourseRegistration;
 import space.bum.jpa.n2n.entity.Student;
 import space.bum.jpa.n2n.repository.CourseRatingRepository;
 import space.bum.jpa.n2n.repository.CourseRepository;
+import space.bum.jpa.n2n.repository.RegistrationRepository;
 import space.bum.jpa.n2n.repository.StudentRepository;
 
 @Controller
@@ -21,6 +25,7 @@ public class StudentController {
   private StudentRepository studentRepository;
   private CourseRepository courseRepository;
   private CourseRatingRepository courseRatingRepository;
+  private RegistrationRepository registrationRepository;
 
   @GetMapping("/")
   public String showHome() {
@@ -33,7 +38,14 @@ public class StudentController {
     var crKey = new CourseRatingKey(student.getId(), course.getId());
     var courseRating = new CourseRating(crKey, student, course, 5);
     courseRatingRepository.save(courseRating);
-    
+
+    var regis = new CourseRegistration();
+    regis.setCourse(course);
+    regis.setStudent(student);
+    regis.setRegisteredAt(LocalDateTime.now());
+    regis.setGrade(3);
+    registrationRepository.save(regis);
+
     return "home";
   }
 
